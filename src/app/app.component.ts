58b2218c01ca7a1deded5915f3f9779b8c15e3f3
Andrei Rechkov <59-andrei.rechkov@users.noreply.gitlab.roonyx.team>
2021-04-23 12:14:38 +0300

    Custom search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject} from 'rxjs';
+import { debounceTime, distinctUntilChanged, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'custom-search';
+export class AppComponent implements OnInit, OnDestroy {
+  public searchInput$ = new Subject();
+  public container$ = new Subject();
+  public data: Array<{ id: number; name: string; image: string}> = [
+    { id: 1, name: 'Test 1', image: 'assets/logo.png'},
+    { id: 2, name: 'Test 2', image: 'assets/logo.png'},
+    { id: 3, name: 'Test 3', image: 'assets/logo.png'},
+    { id: 4, name: 'Test 4', image: 'assets/logo.png'},
+    { id: 5, name: 'Test 5', image: 'assets/logo.png'},
+    { id: 6, name: 'Test 6', image: 'assets/logo.png'},
+    { id: 7, name: 'Test 7', image: 'assets/logo.png'},
+    { id: 8, name: 'Test 8', image: 'assets/logo.png'},
+    { id: 9, name: 'Test 9', image: 'assets/logo.png'},
+  ];
+  public foundData: Array<{ id: number; name: string; image: string}> = [];
+  public showListSearch = false;
+  public exist = false;
+
+  private destroy$ = new Subject();
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.searchInput$.pipe(takeUntil(this.destroy$), debounceTime(300), distinctUntilChanged())
+      .subscribe((title: any) => this.searchResult(title.target.value.toLowerCase()));
+
+    this.container$.pipe(takeUntil(this.destroy$)).subscribe((event: any) => {
+      this.showListSearch = event.type === 'focus';
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  private searchResult(name: string): void {
+    this.foundData = this.data.filter(item => item.name.toLowerCase().includes(name));
+    this.exist = !this.foundData.length;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MdbModule } from 'mdb-angular-ui-kit';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MdbModule,
+    FormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
